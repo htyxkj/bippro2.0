a9c3b69f7e3307afd4ad035328c8b6987c37a505
LiZhengguo <lzg163email@163.com>
2019-11-05 15:59:09 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -111,12 +111,12 @@ export default new Router({
         {
             path: '/',
             name: 'Index',
-            component: Dashboard
+            component: WxApplets
         },
         {
             path: '/index',
             name: 'Dashbord',
-            component: Dashboard,
+            component: WxApplets,
         },
         {
             path: '/task',
