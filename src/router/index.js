78627022db45969faed8eb4ae7311bff34f13421
LiZhengguo <lzg163email@163.com>
2020-01-19 12:13:41 +0800

    修改首页路由

--- src/router/index.js
+++ src/router/index.js
@@ -116,12 +116,12 @@ export default new Router({
         {
             path: '/',
             name: 'Index',
-            component: Dashboard
+            component: WxApplets
         },
         {
             path: '/index',
             name: 'Dashbord',
-            component: Dashboard,
+            component: WxApplets,
         },
         {
             path: '/task',
